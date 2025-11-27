912ddcf1878e2f6cf88af597f4569a16f196aac7
MUHAMMMEDYILMAZ <mohammad.jamous@ogr.sakarya.edu.tr>
2025-11-27 20:41:28 +0300

    update

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongo";
 import { ObjectId } from "mongodb";
